e93b1b23e8729be351adb7ed459fc41c9b6eafef
Bhavin Gunjariya <bhaving1112@gmail.com>
2025-10-06 10:36:07 +0530

    Changes in portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/bhavin-02/",
+  base: "/",
   build: {
     outDir: "dist",
     emptyOutDir: true,
